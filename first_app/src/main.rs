5f345206f94c50c47ef193883db44a659bd578a5
sidiik <sidiikpro@gmail.com>
2023-01-16 17:46:02 +0300

    done part 3

--- first_app/src/main.rs
+++ first_app/src/main.rs
@@ -1,6 +1,32 @@
 fn main() {
-    println!("Hello world");
-    printing::announce_time();
+    // println!("Hello world");
+    // println!("U+273F");
+    // printing::announce_time();
+
+    // struct secrets {
+    //     x: i32,
+    //     y: i32,
+    // }
+
+    // let x_and_y = secrets { x: 1, y: 2 };
+
+    // println!("x is {} and y is {}", x_and_y.x, x_and_y.y)
+
+    // let x_and_y = [12, -89, 45];
+
+    // println!("{:?}", x_and_y)
+    #[derive(Debug)]
+    #[allow(dead_code)]
+
+    enum Fruit {
+        Apple,
+        Banana,
+    }
+
+    type Food = Fruit;
+    let fruit = Food::Banana;
+
+    println!("{:?}", fruit);
 }
 
 mod printing {
