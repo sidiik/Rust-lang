fbb892716a5146bbb46ac04ebfe9697ebc98c248
sidiik <sidiikpro@gmail.com>
2023-01-31 23:22:08 +0300

    Debug traits

--- rustbyexample/structures/src/main.rs
+++ rustbyexample/structures/src/main.rs
@@ -5,6 +5,20 @@ struct Person {
     age: u8,
 }
 
+#[derive(Debug)]
+
+struct Point(f64, f64, f64);
+
+impl Point {
+    fn describe(&self) {
+        println!("This point contains {}, {}, {}", self.0, self.1, self.2)
+    }
+
+    fn twice(&self) -> Point {
+        Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
+    }
+}
+
 fn create_user(name: String, age: u8) -> Person {
     Person { name, age }
 }
@@ -12,9 +26,18 @@ fn create_user(name: String, age: u8) -> Person {
 fn main() {
     println!("Hello, world!");
     let person = create_user("sidiq".to_string(), 15);
+    let _person2 = Person {
+        name: "Ahmed".to_string(),
+        age: 34,
+    };
 
     println!(
         "My name is {} and i am {} years old",
         person.name, person.age
-    )
+    );
+
+    let p = Point(1.0, 2.0, 3.3);
+
+    p.describe();
+    println!("The square of points are {:?}", p.twice())
 }
