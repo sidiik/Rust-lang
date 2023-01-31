ce211ae3ec702255164cde286c87b79c7b28b531
sidiik <sidiikpro@gmail.com>
2023-01-31 23:29:57 +0300

    Added non-method associated function

--- rustbyexample/structures/src/main.rs
+++ rustbyexample/structures/src/main.rs
@@ -17,6 +17,10 @@ impl Point {
     fn twice(&self) -> Point {
         Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
     }
+
+    fn zeros() -> Point {
+        Point(0.0, 0.0, 0.0)
+    }
 }
 
 fn create_user(name: String, age: u8) -> Person {
@@ -39,5 +43,9 @@ fn main() {
     let p = Point(1.0, 2.0, 3.3);
 
     p.describe();
-    println!("The square of points are {:?}", p.twice())
+    println!("The square of points are {:?}", p.twice());
+
+    let p1 = Point::zeros();
+
+    println!("{:?}", p1);
 }
