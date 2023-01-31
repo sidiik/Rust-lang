6d39791d5daac4b08ca4c59a3de8c2c9b57dbcf9
sidiik <sidiikpro@gmail.com>
2023-01-31 20:52:02 +0300

    done

--- rustbyexample/functions/src/main.rs
+++ rustbyexample/functions/src/main.rs
@@ -1,3 +1,12 @@
+#![deny(clippy::all)]
+
 fn main() {
-    println!("Hello, world!");
+    // let _say_hello = |name: &str| format!("Hello, {}", name);
+    fn _say_hello(name: &mut str) -> String {
+        format!("Hello, {}", name)
+    }
+
+    let mut name = String::from("John");
+
+    println!("{} ", _say_hello(&mut name))
 }
