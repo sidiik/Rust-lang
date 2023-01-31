0b7536b4703aff06f798ccbd427f07d65109f339
sidiik <sidiikpro@gmail.com>
2023-01-31 21:08:58 +0300

    pipes in rust functions

--- rustbyexample/functions/src/main.rs
+++ rustbyexample/functions/src/main.rs
@@ -2,11 +2,17 @@
 
 fn main() {
     // let _say_hello = |name: &str| format!("Hello, {}", name);
-    fn _say_hello(name: &mut str) -> String {
-        format!("Hello, {}", name)
-    }
+    // fn _say_hello(name: &mut str) -> String {
+    //     format!("Hello, {}", name)
+    // }
 
-    let mut name = String::from("John");
+    // let mut name = String::from("John");
 
-    println!("{} ", _say_hello(&mut name))
+    // println!("{} ", _say_hello(&mut name))
+
+    // let multiply_by_2 = |x: f32| x * 2.0;
+
+    // let y = 8f32;
+
+    // println!("4 * 2 = {}", multiply_by_2(y));
 }
