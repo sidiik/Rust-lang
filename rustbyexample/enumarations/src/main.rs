daa110a34767893813865ddb2ac32468922f1544
sidiik <sidiikpro@gmail.com>
2023-02-01 21:03:07 +0300

    Enumarations

--- rustbyexample/enumarations/src/main.rs
+++ rustbyexample/enumarations/src/main.rs
@@ -1,10 +1,15 @@
 #![deny(clippy::all)]
-#![warn(dead_code)]
 
 enum AnimalType {
     Dog,
     Cat,
     Rabbit,
+}
+
+#[warn(dead_code)]
+
+enum Shapes {
+    Regtangle { width: f64, height: f64 },
 }
 
 fn main() {
@@ -14,5 +19,17 @@ fn main() {
         AnimalType::Cat => println!("Meow"),
         AnimalType::Dog => println!("Woof"),
         AnimalType::Rabbit => println!("Hoot"),
+    }
+
+    let rectangle = Shapes::Regtangle {
+        width: 3.0,
+        height: 3.4,
+    };
+
+    match rectangle {
+        Shapes::Regtangle { width, height } => {
+            println!("width  {} * height {} = {}", width, height, width * height)
+        }
+        _ => println!("Not a rectange"),
     }
 }
