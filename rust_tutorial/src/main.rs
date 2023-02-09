26ef4c74b1df67546920fbf4a5fe2640171581fc
sidiik <sidiikpro@gmail>
2023-02-09 20:19:20 +0300

    initial commit

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let city_name = "Hargeisa";
+    println!("The city of {}", city_name);
+
+    print_population(1_324_578, 114_293, 108_097)
+}
+
+fn print_population(adults: u64, kids: u32, buildings: u32) {
+    let population = adults + (kids as u64);
+
+    let buildings_per_person = (buildings as f64) / (population as f64);
+
+    println!("Population {}", population);
+    println!("Kids {}", kids);
+    println!("Adults {}", adults);
+    println!("Buildings {}", buildings);
+    println!("Buildings per person {}", buildings_per_person);
 }
